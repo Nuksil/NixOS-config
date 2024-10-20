047338abe8208fea566cf05f386b6f970ef07895
Jarosalw Bochman <nuksil@googlemail.com>
2024-10-20 15:20:11 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,27 @@
-{ config, pkgs, ... }: 
+
+{ config, pkgs, ... }:
 
 {
   imports =
-    [ 
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
+  networking.hostName = "NukNix";
+
+  # Enable networking
   networking.networkmanager.enable = true;
 
+  # Set your time zone.
   time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_GB.UTF-8";
     LC_IDENTIFICATION = "en_GB.UTF-8";
@@ -26,75 +34,88 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
     variant = "";
   };
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
 
+  # Configure console keymap
   console.keyMap = "uk";
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nuks = {
     isNormalUser = true;
-    description = "Nuksil";
+    description = "nuksil";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
+  
+  # Black sereen fix
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_COURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
 
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    htop
-    git
-    waybar
-    kitty
-    google-chrome
-    rofi-wayland
-    neovim
-    vscode
-  ];
-
-  system.stateVersion = "24.05";
-
-  # Hyprland Configuration
+  # Opengl enable
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  
+  };
+  
+  # hyprland enable
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
+ 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  # Session Variables for Wayland and Electron
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1"; # If your cursor becomes invisible
-    NIXOS_OZONE_WL = "1"; # Hint Electron apps to use Wayland
-  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    git  
+    vscode
+    waybar
+    dunst
+    kitty
+    rofi-wayland
+    libnotify 
+    swww
+    google-chrome
+    vscode
+    neovim 
+  ];
 
-  # NVIDIA Drivers
-  hardware.opengl.enable = true;
-  hardware.nvidia = {
-    modesetting.enable = true; # Most Wayland compositors need
-    package = config.boot.kernelPackages.nvidiaPackages.stable; # Use stable version of NVIDIA drivers
-  };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # List services that you want to enable:
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.modesetting.enable = true;
+  
 
-  # XDG Portals for better desktop integration
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Desktop portals
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
+  
+  # Sound enable
   sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  jack.enable  = true;
- };
-  
- 
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  system.stateVersion = "24.05"; 
 
 }
