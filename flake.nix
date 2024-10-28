f6853c11f06adfe93390a3c6d8f461af91923529
Jarosalw Bochman <nuksil@googlemail.com>
2024-10-28 19:20:54 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+description = "My flake";
+
+inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+};
+
+outputs = { self, nixpkgs, ...}:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+        NukNix = lib.nixosSystem {
+            system = "86_64-linux";
+            modules = [ ./configuration.nix ];
+        };
+    };
+
+
+};
+
+
+
+}
