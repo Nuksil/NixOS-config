8e0f07e11e823515cad36e12ae2e0b159490fd4c
Jarosalw Bochman <nuksil@googlemail.com>
2024-11-12 20:05:54 +0000

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,1 +1,44 @@
+{ config, pkgs, ... }:
 
+
+let
+  myAliases = {
+      ll = "ls -l";
+      ".." = "cd ..";
+  };
+in
+{
+  
+  home.username = "nuks";
+  home.homeDirectory = "/home/nuks";
+
+  
+  home.stateVersion = "24.05"; 
+
+
+  home.packages = [
+  pkgs.fzf 
+  ];
+
+  home.file = {
+    
+   }
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = myAliases;
+  };
+  programs.zsh = { 
+   enable = true;
+   shellAliases = myAliases;
+  };
+  
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
+}
